acd9af2c4b46515ca8b6aa42a7db0d951e9892f6
Nour-2310 <nour.mohamed22d@eslsca.edu.eg>
2025-06-02 22:41:33 +0300

    car

--- Assets/Scripts/VehiclePlacer.cs
+++ Assets/Scripts/VehiclePlacer.cs
@@ -5,31 +5,37 @@ using UnityEngine.XR.ARSubsystems;
 
 public class VehiclePlacer : MonoBehaviour
 {
+    [Header("AR Setup")]
     public ARRaycastManager raycastManager;
+
+    [Header("Vehicle")]
     public GameObject carPrefab;
 
     private GameObject placedCar;
+    private static List<ARRaycastHit> hits = new List<ARRaycastHit>();
 
     void Update()
     {
-        if (Input.touchCount > 0)
-        {
-            Touch touch = Input.GetTouch(0);
-            if (touch.phase == TouchPhase.Began)
-            {
-                List<ARRaycastHit> hits = new List<ARRaycastHit>();
-                if (raycastManager.Raycast(touch.position, hits, TrackableType.PlaneWithinPolygon))
-                {
-                    Pose hitPose = hits[0].pose;
+        // Only respond to one-finger touch
+        if (Input.touchCount == 0 || raycastManager == null || carPrefab == null)
+            return;
 
-                    if (placedCar == null)
-                    {
-                        placedCar = Instantiate(carPrefab, hitPose.position, hitPose.rotation);
-                    }
-                    else
-                    {
-                        placedCar.transform.position = hitPose.position;
-                    }
+        Touch touch = Input.GetTouch(0);
+
+        // Only respond when touch begins
+        if (touch.phase == TouchPhase.Began)
+        {
+            if (raycastManager.Raycast(touch.position, hits, TrackableType.PlaneWithinPolygon))
+            {
+                Pose hitPose = hits[0].pose;
+
+                if (placedCar == null)
+                {
+                    placedCar = Instantiate(carPrefab, hitPose.position, hitPose.rotation);
+                }
+                else
+                {
+                    placedCar.transform.position = hitPose.position;
                 }
             }
         }
