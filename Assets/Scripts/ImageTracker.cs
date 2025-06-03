78fbdc0e5f29317819de55f5f5981b874d156162
Nour-2310 <nour.mohamed22d@eslsca.edu.eg>
2025-06-04 01:38:57 +0300

    updates

---
+++ Assets/Scripts/ImageTracker.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+public class ImageTracker : MonoBehaviour
+{
+    private ARTrackedImageManager trackedImages;
+    public GameObject[] ArPrefabs;
+    List<GameObject> ARObjects = new List<GameObject>();
+
+    void Awake()
+    {
+        trackedImages = GetComponent<ARTrackedImageManager>();
+    }
+    void OnEnable()
+    {
+        trackedImages.trackedImagesChanged += OnTrackedImagesChanged;
+    }
+    void OnDisable()
+    {
+        trackedImages.trackedImagesChanged -= OnTrackedImagesChanged;
+    }
+    // Event Handler
+    private void OnTrackedImagesChanged(ARTrackedImagesChangedEventArgs eventArgs)
+    {
+        //Create object based on image tracked
+        foreach (var trackedImage in eventArgs.added)
+        {
+            foreach (var arPrefab in ArPrefabs)
+            {
+                if (trackedImage.referenceImage.name == arPrefab.name)
+                {
+                    var newPrefab = Instantiate(arPrefab, trackedImage.transform);
+                    ARObjects.Add(newPrefab);
+                }
+            }
+        }
+    }
+}
